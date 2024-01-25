741c1dc6ee201175d59dc5813f0b2c8293dab683
Rafael Jáber <rafael.jaber@hotmail.com>
2024-01-25 11:03:16 -0300

    Adicionado o componente LeftBar ao PlayerPage, configurada a rota para o componente PlayerPageComponent

--- src/app/web/pages/player-page/player-page.component.ts
+++ src/app/web/pages/player-page/player-page.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { LeftBarComponent } from '../../components/left-bar/left-bar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-player-page',
   standalone: true,
-  imports: [],
+  imports: [LeftBarComponent, RouterOutlet],
   templateUrl: './player-page.component.html',
-  styleUrl: './player-page.component.css'
+  styleUrl: './player-page.component.css',
 })
-export class PlayerPageComponent {
-
-}
+export class PlayerPageComponent {}
--- src/app/web/pages/player-page/player-page.routes.ts
+++ src/app/web/pages/player-page/player-page.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from '../home-page/home-page.component';
+import { PlayerPageComponent } from './player-page.component';
 
 export const PLAYER_PAGE_ROUTES: Routes = [
   {
     path: '',
+    component: PlayerPageComponent,
     children: [
       {
         path: 'home',
