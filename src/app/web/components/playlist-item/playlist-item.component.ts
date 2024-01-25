6d57553f16a8c0a8f7dae386ee6d75885b04bb1e
Rafael Jáber <rafael.jaber@hotmail.com>
2024-01-25 20:59:44 -0300

    Implementada a sessão das 6 principáis playlists, para total funcionalidade e reaproveitamento dos componentes foi criado o PlaylistItem.

---
+++ src/app/web/components/playlist-item/playlist-item.component.ts
@@ -0,0 +1,18 @@
+import { booleanAttribute, Component, Input } from '@angular/core';
+import { IPlaylistModel } from '../../../domain/interfaces/IPlaylist.model';
+import { newPlaylist } from '../../../core/utils/factories';
+
+@Component({
+  selector: 'app-playlist-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './playlist-item.component.html',
+  styleUrl: './playlist-item.component.css',
+})
+export class PlaylistItemComponent {
+  @Input({ transform: booleanAttribute })
+  isPlaying: boolean = false;
+
+  @Input()
+  playlist: IPlaylistModel = newPlaylist();
+}
--- src/app/web/pages/home-page/home-page.component.ts
+++ src/app/web/pages/home-page/home-page.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlaylistItemComponent } from '../../components/playlist-item/playlist-item.component';
+import { IPlaylistModel } from '../../../domain/interfaces/IPlaylist.model';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [PlaylistItemComponent],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrl: './home-page.component.css',
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  protected playlists: IPlaylistModel[] = [];
 
+  ngOnInit(): void {
+    this.init();
+  }
+
+  private init() {
+    for (let i = 0; i < 6; i++) {
+      const prov: IPlaylistModel = {
+        id: i.toString(),
+        name: 'Playlist ' + i.toString(),
+        imageUrl: 'https://github.com.br',
+      };
+      this.playlists.push(prov);
+    }
+  }
 }
