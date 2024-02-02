d3e0c54ac8fa71678fdac2d34deef919a2ecb43a
Rafael Jáber <rafael.jaber@hotmail.com>
2024-02-02 00:12:07 -0300

    [Mini Player] Atualiza o componente de mini player para exibir corretamente as informações da música atual. Adiciona a lógica para reproduzir, pausar, avançar e retroceder as faixas. Refatora e otimiza o código para melhor desempenho.

--- src/app/web/components/mini-player/mini-player.component.ts
+++ src/app/web/components/mini-player/mini-player.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { PlayerService } from '@domain/services/player.service';
 
 @Component({
   selector: 'app-mini-player',
   standalone: true,
   imports: [],
   templateUrl: './mini-player.component.html',
-  styleUrl: './mini-player.component.css'
+  styleUrl: './mini-player.component.css',
 })
-export class MiniPlayerComponent {
+export class MiniPlayerComponent implements OnInit {
+  private playerService = inject(PlayerService);
 
+  protected playerStateSignal = this.playerService.getPlayerState();
+
+  ngOnInit() {
+    this.playerService.initPlayer().then();
+  }
+
+  async onTogglePlay() {
+    await this.playerService.handleTogglePlay();
+  }
+
+  async onNextClick() {
+    await this.playerService.handleNextTrack();
+  }
+
+  async onPreviousClick() {
+    await this.playerService.handlePreviousTrack();
+  }
 }
