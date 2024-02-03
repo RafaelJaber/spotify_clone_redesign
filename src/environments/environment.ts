0d8cca58628f075bbac2a873f7a472a3804e6d9a
Rafael Jáber <rafael.jaber@hotmail.com>
2024-02-03 15:51:33 -0300

    [environment.ts, environment.ts, @@ -1,1 +1,19 @@, -export const environment = {};, +export const environment = {, +  authEndpoint: 'https://accounts.spotify.com/authorize',, +  redirectUri: 'https://spotify-clone-redesign-tau.vercel.app/login/',, +  scopes: [, +    'user-read-currently-playing',, +    'user-read-recently-played',, +    'user-read-playback-state',, +    'user-top-read',, +    'user-modify-playback-state',, +    'user-library-read',, +    'playlist-read-private',, +    'playlist-read-collaborative',, +    'user-follow-read',, +    'user-library-modify',, +    'user-read-private',, +    'user-read-email',, +    'streaming',,  ],, +};]

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,19 @@
-export const environment = {};
+export const environment = {
+  authEndpoint: 'https://accounts.spotify.com/authorize',
+  redirectUri: 'https://spotify-clone-redesign-tau.vercel.app/login/',
+  scopes: [
+    'user-read-currently-playing',
+    'user-read-recently-played',
+    'user-read-playback-state',
+    'user-top-read',
+    'user-modify-playback-state',
+    'user-library-read',
+    'playlist-read-private',
+    'playlist-read-collaborative',
+    'user-follow-read',
+    'user-library-modify',
+    'user-read-private',
+    'user-read-email',
+    'streaming',
+  ],
+};
